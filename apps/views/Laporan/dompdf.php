cfecade5746da21cd9ef1fe47eb476ce9dcff590
Raihan1801 <abualiraihan1801@gmail.com>
2022-02-22 11:05:27 +0700

    View Surat

---
+++ apps/views/Laporan/dompdf.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Laporan</title>
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        p {
+            line-height: 0.1;
+            font-size: 10px;
+            text-align: right;
+        }
+
+        h6 {
+            line-height: 0.1;
+            font-size: 13;
+            text-align: right;
+        }
+
+        hr {
+            border-bottom: solid;
+            border-top: 0px;
+            border-left: 0px;
+            border-right: 0px;
+            color: black;
+            margin-top: 0px;
+        }
+
+        table tr .text2 {
+            text-align: center;
+        }
+
+        table tr .text3 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <table>
+            <tr>
+                <td><img src="assets/logo.png" width="250" height="85"></td>
+                <td width="25">
+                </td>
+                <td class="text" width="300" height="100"><br>
+                    <h6>SURABAYA BARAT</h6>
+                    <p>Jl. Tubanan Baru 10 Blok K-15 Surabaya 60188, East Java, Indonesia.</p>
+                    <p>Telp. 031-7328 540, 031-99020730 | Mobile. 0821 3131 4040</p>
+                    <h6>SURABAYA TIMUR</h6>
+                    <p>Jl. Nginden Intan Timur XVIII A3-10 Surabaya 60118, East Java, Indonesia</p>
+                    <p>Telp. 031-5917 3739 | Mobile. 0821 3131 0210</p>
+                    <p><a href="mailto:care@creativemedia.id" target="_blank">care@creativemedia.id</a> | <a href="https://creativemedia.id/">www.creativemedia.id</a></p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <hr>
+                </td>
+            </tr>
+        </table>
+        <table width="100%" height="0">
+            <tr>
+                <td class="text" style="text-align: center; "><b>Surat Izin Cuti</b></td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td>Nomer</td>
+                <td width="572">: </td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td width="572">: </td>
+            </tr>
+            <tr>
+                <td>Jabatan</td>
+                <td width="572">: </td>
+            </tr>
+            <tr>
+                <td>Divisi</td>
+                <td width="572">: </td>
+            </tr>
+            <tr>
+                <td>Tanggal Cuti</td>
+                <td width="572">: </td>
+            </tr>
+            <tr>
+                <td>Jenis Cuti</td>
+                <td width="572">: </td>
+            </tr>
+            <tr>
+                <td>Sisa Cuti</td>
+                <td width="572">: </td>
+            </tr>
+            <tr>
+                <td>Keperluan</td>
+                <td width="572">: </td>
+            </tr>
+            <br><br>
+        </table><br>
+        <table width="100%" style="text-align: left;">
+            <tr>
+                <td width="100"></td>
+                <td width="100"></td>
+                <td width="85">Surabaya, </td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td width="200" class="text2" align="">Mengetahui<br><b>Admin & HRD</b><br><br><br><br><br><b><u>Dea Oktavia Putri Aryani, S.H.</u></b><br><b>(NIK. 210901)</b></td>
+                <td width="100"></td>
+                <td width="200" class="text2" align="">Mengetahui<br><b>Manager HR & GA</b><br><br><br><br><br><b><u>Tsatyana Ultah, S.Pd</u></b><br><b>(NIK. 200801)</b></td>
+            </tr>
+        </table><br><br>
+        <table>
+            <tr>
+                <td width="100" align=""></td>
+                <td width="300" class="text3" align="">Disetujui,<br><b>Direktur</b><br><br><br><br><br><b><u>Hadi Sisyanto, S,Kom</u></b><br><b>(NIK. 141001)</b></td>
+                <td width="100" align=""></td>
+            </tr>
+        </table>
+    </center>
+</body>
+
+</html>
